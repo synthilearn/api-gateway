d016608e23fc49a22ca7021a46ccffcb20388528
Nikita Porshennikov <nporshennikov@sharespot.pro>
2024-02-07 14:02:10 +0700

    Added token validation

--- src/main/java/com/synthilearn/gatewayservice/filters/AuthFilter.java
+++ src/main/java/com/synthilearn/gatewayservice/filters/AuthFilter.java
@@ -1,25 +1,99 @@
 package com.synthilearn.gatewayservice.filters;
 
+import io.jsonwebtoken.Claims;
+import io.jsonwebtoken.Jws;
+import io.jsonwebtoken.Jwts;
+import jakarta.annotation.PostConstruct;
+import lombok.SneakyThrows;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.cloud.gateway.filter.GatewayFilter;
 import org.springframework.cloud.gateway.filter.factory.AbstractGatewayFilterFactory;
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.server.reactive.ServerHttpRequest;
 import org.springframework.stereotype.Component;
+import org.springframework.web.server.ServerWebExchange;
+import reactor.core.publisher.Mono;
+
+import java.security.KeyFactory;
+import java.security.PublicKey;
+import java.security.interfaces.RSAPublicKey;
+import java.security.spec.X509EncodedKeySpec;
+import java.util.Base64;
+import java.util.List;
 
 @Slf4j
 @Component
+@RefreshScope
 public class AuthFilter extends AbstractGatewayFilterFactory<AuthFilter.Config> {
+
+    @Value("${security.allowed-addresses}")
+    private List<String> allowedAddresses;
+    @Value("${security.public-key}")
+    private String publicKeyString;
+    private RSAPublicKey publicKey;
 
     public AuthFilter() {
         super(Config.class);
     }
 
+    @PostConstruct
+    public void init() {
+        this.publicKey = (RSAPublicKey) getPublicKeyFromPEM(publicKeyString);
+    }
+
     @Override
     public GatewayFilter apply(AuthFilter.Config config) {
         return ((exchange, chain) -> {
+            ServerHttpRequest request = exchange.getRequest();
+            String currentPath = request.getPath().toString();
+            if (allowedAddresses.stream().anyMatch(currentPath::contains)) {
+                return chain.filter(exchange);
+            }
+
+            String token = extractToken(request);
+            if ((token == null || token.isEmpty()) || !validateToken(token)) {
+                return unauthorized(exchange);
+            }
 
             return chain.filter(exchange);
         });
     }
+
+    private String extractToken(ServerHttpRequest request) {
+        String authHeader = request.getHeaders().getFirst(HttpHeaders.AUTHORIZATION);
+        if (authHeader != null && authHeader.startsWith("Bearer ")) {
+            return authHeader.substring(7);
+        }
+        return null;
+    }
+
+    private boolean validateToken(String token) {
+        try {
+            Jws<Claims> claims = Jwts.parser().setSigningKey(publicKey).parseClaimsJws(token);
+            return true;
+        } catch (Exception e) {
+            log.error("Token validation failed: {}", e.getMessage());
+            return false;
+        }
+    }
+
+    @SneakyThrows
+    private PublicKey getPublicKeyFromPEM(String publicKeyPEM) {
+        byte[] publicKeyBytes = Base64.getDecoder().decode(publicKeyPEM);
+
+        KeyFactory keyFactory = KeyFactory.getInstance("RSA");
+        X509EncodedKeySpec keySpec = new X509EncodedKeySpec(publicKeyBytes);
+        return keyFactory.generatePublic(keySpec);
+    }
+
+    private Mono<Void> unauthorized(ServerWebExchange exchange) {
+        exchange.getResponse().setStatusCode(HttpStatus.UNAUTHORIZED);
+        return exchange.getResponse().setComplete();
+    }
+
 
     public static class Config {
 
